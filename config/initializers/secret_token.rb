bce97d17b5e3d1e418ccab15a78dd762b291dedf
Matthew Crouch <matthew.a.crouch@gmail.com>
2013-11-14 21:51:17 -0500

    Initial commit!
    with rspec, figaro, bootstrap, and guard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChristmasLists::Application.config.secret_key_base = '70166f1ef4df21b8b288f55e2b7c150bba135620b7e16af8bef9535b2d523f6ce9a5d225463a6e712e881a941d4a348cc51bb6b358b4ac57fc3b1f90512cc74f'
