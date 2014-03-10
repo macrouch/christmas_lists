ca28eddf474af7ee5fd63e0d123619a07228d5f8
Matthew Crouch <matthew.a.crouch@gmail.com>
2014-03-10 18:43:12 -0400

    fixing missing images in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ ChristmasLists::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # precompile images
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
