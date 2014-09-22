f556c6eaff498d6f4c7836079f3cb7b652b2f28b
Matthew Crouch <matthew.a.crouch@gmail.com>
2014-09-21 20:57:25 -0400

    display first part of URLs in item descriptions

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,2 +1,15 @@
 module CollectionsHelper
+  def auto_link_text(text)
+    auto_link(text) do |url|
+      short_url(url)
+    end
+  end
+
+  def short_url(url)
+    if url.length > 30
+      url[0..29] + '...'
+    else
+      url
+    end
+  end
 end
