a4ed885e2765ee19f9565b144c22178434ab1f75
Matthew Crouch <matthew.a.crouch@gmail.com>
2014-10-29 16:58:57 -0400

    trying to fix csrf token failure on iOS

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :identity, on_failed_registration: lambda { |env|
-    IdentitiesController.action(:new).call(env)
-  }
+  provider :identity,
+    on_login: -> (env) { SessionsController.action(:new).call(env) },
+    on_registration: -> (env) { IdentitiesController.action(:new).call(env) },
+    on_failed_registration: -> (env) { IdentitiesController.action(:new).call(env) }
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
