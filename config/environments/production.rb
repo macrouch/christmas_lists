bca347e0a30e973a3387a179a5afd90b873a79e9
Matthew Crouch <matthew.a.crouch@gmail.com>
2018-12-11 23:42:26 -0500

    move paperclip images back to local storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,14 +87,4 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: 'christmas.sleekcoder.com' }
-
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_region: ENV['S3_REGION'],
-    s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end
